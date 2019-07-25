8f631ec7f9b60ed71bdd39f7355a61778a3b2248
夏许强 <xiaxuqiang@changingedu.com>
2019-07-26 01:19:00 +0800

    OC、Swift 混编配置

---
+++ Hybride/OCSwiftApp/OCSwiftApp/TestModel.swift
@@ -0,0 +1,9 @@
+//
+//  TestModel.swift
+//  OCSwiftApp
+//
+//  Created by Ben on 2019/7/26.
+//  Copyright © 2019 iOSStudio. All rights reserved.
+//
+
+import Foundation
--- Hybride/OCSwiftApp/OCSwiftApp/ViewController.m
+++ Hybride/OCSwiftApp/OCSwiftApp/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "OCSwiftApp-Swift.h"
 
 @interface ViewController ()
 
