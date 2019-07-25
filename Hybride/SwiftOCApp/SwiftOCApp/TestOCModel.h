ec99d5e03c5f8d634e7eb4e4c213394172a69357
夏许强 <xiaxuqiang@changingedu.com>
2019-07-26 01:24:54 +0800

    Swift、OC 混编配置

---
+++ Hybride/SwiftOCApp/SwiftOCApp/TestOCModel.h
@@ -0,0 +1,17 @@
+//
+//  TestOCModel.h
+//  SwiftOCApp
+//
+//  Created by Ben on 2019/7/26.
+//  Copyright © 2019 iOSStudio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestOCModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Hybride/SwiftOCApp/SwiftOCApp/TestOCModel.m
@@ -0,0 +1,14 @@
+//
+//  TestOCModel.m
+//  SwiftOCApp
+//
+//  Created by Ben on 2019/7/26.
+//  Copyright © 2019 iOSStudio. All rights reserved.
+//
+
+#import "TestOCModel.h"
+#import "SwiftOCApp-Swift.h"
+
+@implementation TestOCModel
+
+@end
